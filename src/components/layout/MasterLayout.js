98d416c1142faadb0f481555b785a92a23a014ac
Jose Aguilar <manugola24@gmail.com>
2020-09-12 19:46:48 -0300

    upgrade master layout

--- src/components/layout/MasterLayout.js
+++ src/components/layout/MasterLayout.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import clsx from 'clsx';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import Box from '@material-ui/core/Box';
@@ -7,12 +8,25 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Drawer from '@material-ui/core/Drawer';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import SearchInput from '../common/SearchInput'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { mainListItems, secondaryListItems } from '../dashboard/listItems'
+import BannerLogo from '../../assets/images/sice-logo.png'
+
 import Copyright from '../common/Copyright'
 import { setLogOut } from '../../actions/account'
 import { PersistorContext } from '../../context/persistorContext'
 import { useHistory } from "react-router-dom";
 import { useStyles } from './masterLayoutStyles'
-import MasterLayoutToolbar from './MasterLayoutComponents/MLToolbar'
 import MasterLayoutBanner from './MasterLayoutComponents/MLBanner'
 import MasterLayoutAside from './MasterLayoutComponents/MLAside'
 
@@ -42,7 +56,61 @@ const MasterLayout = ({ children, render, logOut, account, ...rest }) => {
 
   return (
     <div className={classes.root}>
-      <MasterLayoutToolbar open={open} classes={classes} onCloseDrawer={handleDrawerClose} onOpenDrawer={handleDrawerOpen} onAsideOpen={handleAsideOpen} aside={aside} />
+    <>
+      <CssBaseline />
+      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+        <Toolbar className={classes.toolbar}>
+          <Grid container alignItems='center'>
+            <Grid item xs={open ? false : 1}>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+              >
+                <MenuIcon style={{ color: '#f17f2d' }} />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <SearchInput open={open} />
+            </Grid>
+          </Grid>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                  <NotificationsIcon style={{ color: 'grey' }} />
+                </Badge>
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <IconButton onClick={handleAsideOpen} color="inherit">
+                <AccountCircleIcon style={{ color: 'grey' }} />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      </>
+      <Drawer
+            variant="permanent"
+            classes={{
+                paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+            }}
+            open={open}
+        >
+            <div className={classes.toolbarIcon}>
+                <img style={{ height: 35, marginTop: 4 }} alt='sice logo' src={BannerLogo} />
+                <IconButton onClick={handleDrawerClose}>
+                    <ChevronLeftIcon style={{ color: '#f17f2d' }} />
+                </IconButton>
+            </div>
+            <Divider />
+            <List>{mainListItems}</List>
+            <Divider />
+            <List>{secondaryListItems}</List>
+        </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <MasterLayoutBanner classes={classes} />
