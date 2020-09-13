9036d04ecbcb790fd6abfd034cee43c0199b83b9
Jose Aguilar <manugola24@gmail.com>
2020-09-12 21:15:50 -0300

    add mobile layout

--- src/components/layout/MasterLayout.js
+++ src/components/layout/MasterLayout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import clsx from 'clsx';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -21,7 +21,7 @@ import Divider from '@material-ui/core/Divider';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { ListItems } from '../dashboard/listItems'
 import BannerLogo from '../../assets/images/sice-logo.png'
-
+import { useTheme } from '@material-ui/core/styles';
 import Copyright from '../common/Copyright'
 import { setLogOut } from '../../actions/account'
 import { PersistorContext } from '../../context/persistorContext'
@@ -29,7 +29,7 @@ import { useHistory } from "react-router-dom";
 import { useStyles } from './masterLayoutStyles'
 import MasterLayoutBanner from './MasterLayoutComponents/MLBanner'
 import MasterLayoutAside from './MasterLayoutComponents/MLAside'
-
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { useAccount } from '../../hooks/user'
 
 const MasterLayout = ({ children, render, logOut, account, ...rest }) => {
@@ -45,84 +45,112 @@ const MasterLayout = ({ children, render, logOut, account, ...rest }) => {
   };
 
   const handleAsideOpen = () => {
-      setAside(!aside)
-      if (!aside) {
-        setOpen(aside)
-      }
+    setAside(!aside)
+    if (!aside) {
+      setOpen(aside)
+    }
   }
   const { decrypt } = useAccount()
   const user = decrypt(account)
   const history = useHistory()
   const { purge } = useContext(PersistorContext)
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+  useEffect(() => {
+    if (isMobile) {
+      setOpen(false)
+    }
+  }, [isMobile])
+
+  const MenuItems = () => {
+    return (
+      <>
+        <Divider />
+        <List>
+          <ListItems onChange={setSelected} selected={selected} history={history} items={['Dashboard', 'Cheqs', 'Conciliation', 'Lorem']} />
+        </List>
+        <Divider />
+        <List>
+          <ListItems onChange={setSelected} selected={selected} items={['Reports']} />
+        </List>
+      </>
+    )
+  }
 
   return (
     <div className={classes.root}>
-    <>
-      <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <Grid container alignItems='center'>
-            <Grid item xs={open ? false : 1}>
-              <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-              >
-                <MenuIcon style={{ color: '#f17f2d' }} />
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <SearchInput open={open} />
-            </Grid>
-          </Grid>
-          <Grid container justify='flex-end'>
-            <Grid item>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <NotificationsIcon style={{ color: 'grey' }} />
-                </Badge>
-              </IconButton>
-            </Grid>
-            <Grid item>
-              <IconButton onClick={handleAsideOpen} color="inherit">
-                <AccountCircleIcon style={{ color: 'grey' }} />
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-      </>
-      <Drawer
-            variant="permanent"
-            classes={{
-                paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-            }}
-            open={open}
-        >
-            <div className={classes.toolbarIcon}>
-                <img style={{ height: 35, marginTop: 4 }} alt='sice logo' src={BannerLogo} />
-                <IconButton onClick={handleDrawerClose}>
-                    <ChevronLeftIcon style={{ color: '#f17f2d' }} />
+      <>
+        <CssBaseline />
+        <AppBar position="absolute" className={clsx(classes.appBar, (open && !isMobile) && classes.appBarShift)}>
+          <Toolbar className={classes.toolbar}>
+            <Grid container alignItems='center'>
+              <Grid item xs={(open && !isMobile) ? false : 1}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  className={clsx(classes.menuButton, (open && !isMobile) && classes.menuButtonHidden)}
+                >
+                  <MenuIcon style={{ color: '#f17f2d' }} />
                 </IconButton>
-            </div>
-            <Divider />
-            <List>
-            <ListItems onChange={setSelected} selected={selected} history={history} items={['Dashboard', 'Cheqs', 'Conciliation', 'Lorem']} />
-            </List>
-            <Divider />
-            <List>
-            <ListItems onChange={setSelected} selected={selected} items={['Reports']} />
-            </List>
-        </Drawer>
+              </Grid>
+              <Grid item>
+                {!isMobile && <SearchInput open={open} />}
+              </Grid>
+            </Grid>
+            <Grid container justify='flex-end'>
+              <Grid item>
+                <IconButton color="inherit">
+                  <Badge badgeContent={4} color="secondary">
+                    <NotificationsIcon style={{ color: 'grey' }} />
+                  </Badge>
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <IconButton onClick={handleAsideOpen} color="inherit">
+                  <AccountCircleIcon style={{ color: 'grey' }} />
+                </IconButton>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </>
+      {!isMobile && (
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <img style={{ height: 35, marginTop: 4 }} alt='sice logo' src={BannerLogo} />
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon style={{ color: '#f17f2d' }} />
+            </IconButton>
+          </div>
+          <MenuItems />
+        </Drawer>)}
+      {isMobile && (
+         <Drawer
+         anchor='left'
+         classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+         onClose={() => setOpen(false)}
+         open={open}
+       >
+        <MenuItems />
+       </Drawer>
+      )}
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <MasterLayoutBanner classes={classes} />
+        <MasterLayoutBanner classes={classes} isMobile={isMobile} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
             {render({
-                user
+              user
             })}
           </Grid>
           <Box pt={4}>
--- src/components/layout/MasterLayoutComponents/MLBanner.js
+++ src/components/layout/MasterLayoutComponents/MLBanner.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
@@ -6,8 +6,13 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Grid from '@material-ui/core/Grid';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
-const MasterLayoutBanner = ({ title = 'Lorem Integrations - Order #129882' }) => {
+const MasterLayoutBanner = ({ title = 'Lorem Integrations - Order #129882', isMobile }) => {
   const [open, setOpen] = useState(true)
+  useEffect(() => {
+    if (isMobile) {
+      setOpen(false)
+    }
+  }, [isMobile])
   return (
         <Grid container alignItems='center' justify='space-between' style={{ color: 'white', paddingLeft: open ? 30 : 10, height: open ? 80 : 40, background: 'linear-gradient(45deg, rgb(255, 96, 13) 30%, rgb(247, 170, 55) 90%)'}}>
          <Grid item>
@@ -24,9 +29,9 @@ const MasterLayoutBanner = ({ title = 'Lorem Integrations - Order #129882' }) =>
           </Grid>
          </Grid>
          <Grid item>
-         <IconButton onClick={() => setOpen(!open)}>
+         {!isMobile && <IconButton onClick={() => setOpen(!open)}>
            {open ? <ExpandLessIcon style={{ color: 'white'}} fontSize='large' /> : <ExpandMoreIcon style={{ color: 'white'}} fontSize='small' /> }
-         </IconButton>
+         </IconButton>}
          </Grid>
         </Grid>
   );
