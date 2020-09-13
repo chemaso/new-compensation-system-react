0c887f3dd333ef3a31517a271d66b66d3cf79842
Jose Aguilar <manugola24@gmail.com>
2020-09-13 16:41:41 -0300

    upgade scrollbar

--- src/theme.js
+++ src/theme.js
@@ -7,6 +7,46 @@ export const theme = createMuiTheme({
     fontFamily: fonts,
   },
   overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '*': {
+          'scrollbar-width': 'thin',
+        },
+        '*::-webkit-scrollbar': {
+          width: '2px',
+          height: '2px',
+        },
+        '*::-webkit-scrollbar-button': {
+          width: '0px',
+          height: '0px'
+        },
+        '*::-webkit-scrollbar-thumb': {
+          background: '#e1e1e1',
+          border: '0px none #ffffff',
+          borderRadius: '50px'
+        },
+        '*::-webkit-scrollbar-thumb:hover': {
+          background: '#ffffff'
+        },
+        '*::-webkit-scrollbar-thumb:active': {
+          background: '#000000'
+        },
+        '*::-webkit-scrollbar-track': {
+          background: '#666666',
+          border: '0px none #ffffff',
+          borderRadius: '50px'
+        },
+        '*::-webkit-scrollbar-track:hover': {
+          background: '#666666',
+        },
+        '*::-webkit-scrollbar-track:active': {
+          background: '#333333',
+        },
+        '*::-webkit-scrollbar-corner': {
+          background: 'transparent',
+        },
+      }
+    },
     MuiButton: {
       root: {
         background: 'linear-gradient(45deg, #f7aa37 30%, #ff600d 90%)',
@@ -18,5 +58,5 @@ export const theme = createMuiTheme({
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       },
     }
-  }
+  },
 });
